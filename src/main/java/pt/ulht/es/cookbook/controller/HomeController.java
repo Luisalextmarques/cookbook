46b588addf8fda5da19353abe8c8b0af82499c7c
Luis Alexandre Tomás Marques <Luisalextmarques@gmail.com>
2013-05-26 23:19:58 +0100

    Check 3 Search

--- src/main/java/pt/ulht/es/cookbook/controller/HomeController.java
+++ src/main/java/pt/ulht/es/cookbook/controller/HomeController.java
@@ -3,7 +3,6 @@ package pt.ulht.es.cookbook.controller;
 import java.text.DateFormat;
 import java.util.Date;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,39 +14,29 @@ import pt.ulht.es.cookbook.domain.CookbookManager;
 
 @Controller
 public class HomeController {
-  
-	@RequestMapping(method=RequestMethod.GET, value="/")
-	public String showHome(Model model) {
-		CookbookManager.getDefaults(model,"home");
-		
-        Date date = new Date(System.currentTimeMillis());
-        DateFormat df = DateFormat.getDateInstance();
-        model.addAttribute("currentTime", df.format(date));
 
-  	  
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String showHome(Model model) {
+		CookbookManager.getDefaults(model, "home");
+
+		Date date = new Date(System.currentTimeMillis());
+		DateFormat df = DateFormat.getDateInstance();
+		model.addAttribute("currentTime", df.format(date));
+
 		return "home";
 	}
-	
-	@RequestMapping(method=RequestMethod.GET, value="/aboutUs")
+
+	@RequestMapping(method = RequestMethod.GET, value = "/aboutUs")
 	public String showAboutUs(Model model) {
 
-		CookbookManager.getDefaults(model,"aboutUs");
+		CookbookManager.getDefaults(model, "aboutUs");
 
-        Date date = new Date(System.currentTimeMillis());
-        DateFormat df = DateFormat.getDateInstance();
-        model.addAttribute("currentTime", df.format(date));
-        
+		Date date = new Date(System.currentTimeMillis());
+		DateFormat df = DateFormat.getDateInstance();
+		model.addAttribute("currentTime", df.format(date));
+
 		return "aboutUs";
 	}
-	
-	
-	@RequestMapping(value = "/search", method = RequestMethod.POST)
-	public String search(@ModelAttribute("search")	String search, BindingResult result,Model model) {
 
-		CookbookManager.getDefaults(model,"searchClass");
-
-		model.addAttribute("search", search);
-		return "search";
-}
 
 }
--- src/main/webapp/WEB-INF/jsp/search.jsp
+++ src/main/webapp/WEB-INF/jsp/search.jsp
@@ -1,8 +1,17 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+﻿<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <%@ include file = "/WEB-INF/jsp/commons/top.jsp" %>
-<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
-
 				<h2 class="title"><a href="#">Search results</a></h2>
-		<br>
-		${search}
+				<br>
+				${search}
+				<br>
+				<ul>
+					<c:forEach var="item" items='${items}'>
+						<li>Receita: <a href="/recipes/${item.getExternalId()}">${item.titulo} </a>
+						<br>${item.problema}
+						</li>
+						
+					</c:forEach>
+				</ul>
 <%@ include file = "/WEB-INF/jsp/commons/bottom.jsp" %>
